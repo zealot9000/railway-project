eb743f72defa2d83ee86a371ec5867b8c22dc594
zealot9000 <zilatov@gmail.com>
2017-01-29 22:28:56 +0300

    Lesson13 1.1

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,6 +1,6 @@
 class Train < ActiveRecord::Base
   belongs_to :route
-  belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id
+  belongs_to :current_station, class_name: 'RailwayStation' #, foreign_key: :current_station_id
   has_many :tickets
 
   validates :number, presence: true
