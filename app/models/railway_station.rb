594a755715cebf0686b29ff885decbe9ffa82b13
zealot <zealot@zealot-2.local>
2017-04-30 14:46:37 +0300

    fix

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -5,7 +5,8 @@ class RailwayStation < ActiveRecord::Base
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
 
-  scope :ordered, -> { joins(:railway_stations_routes).order("railway_stations_routes.position").uniq }
+  # scope :ordered, -> { joins(:railway_stations_routes).order("railway_stations_routes.position").uniq }
+  scope :ordered, -> { select('railway_stations.*, railway_stations_routes.position').joins(:railway_stations_routes).order("railway_stations_routes.position").uniq }
 
   def update_position(route , position)
     station_route = station_route(route)
